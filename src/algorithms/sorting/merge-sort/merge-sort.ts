a00c8cd6b330b35fd8ce3adf03d81a2cd9e6c866
Emmanuel ZANMENOU <emmanueldev920@gmail.com>
2023-11-21 23:50:21 +0100

    feat: implement merge sort algo

--- src/algorithms/sorting/merge-sort/merge-sort.ts
+++ src/algorithms/sorting/merge-sort/merge-sort.ts
@@ -2,6 +2,44 @@ import { Sort } from '../sort';
 
 export class MergeSort<T> extends Sort {
   sort(array: T[]) {
-    throw new Error('Method not implemented.');
+    if (array.length <= 1) {
+      return array;
+    }
+
+    // split array on the middle
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArray = array.slice(0, middleIndex);
+    const rightArray = array.slice(middleIndex);
+
+    // sort two halves of splitted arrays
+    const leftSortedArray = this.sort(leftArray);
+    const rightSortedArray = this.sort(rightArray);
+
+    return this.mergeSortedArray(leftSortedArray, rightSortedArray);
+  }
+
+  mergeSortedArray(leftArray: Array<T>, rightArray: Array<T>) {
+    const sortedArray = [];
+
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      let minElement: T;
+      if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+        minElement = leftArray[leftIndex];
+        leftIndex++;
+      } else {
+        minElement = rightArray[rightIndex];
+        rightIndex++;
+      }
+
+      // add the minimum element to the sorted array
+      sortedArray.push(minElement);
+    }
+
+    return sortedArray
+      .concat(leftArray.splice(leftIndex))
+      .concat(rightArray.slice(rightIndex));
   }
 }
